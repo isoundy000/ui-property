5a329c5e33c9857b9b8880af157b71dd3007a68f
Johnny Wu <KarasAya@gmail.com>
2015-08-13 19:32:43 +0800

    update object property

--- widget/object-prop/object-prop.js
+++ widget/object-prop/object-prop.js
@@ -1,18 +1,150 @@
-Editor.registerWidget( 'editor-array-size', {
-    is: 'editor-array-size',
+Editor.registerWidget( 'editor-object-prop', {
+    is: 'editor-object-prop',
+
+    behaviors: [EditorUI.focusable],
+
+    listeners: {
+        'disabled-changed': '_onDisabledChanged',
+    },
 
     properties: {
+        type: {
+            type: String,
+            value: '',
+        },
+
+        name: {
+            type: String,
+            value: '',
+        },
+
+        attrs: {
+            type: Object,
+            value: function () { return {}; },
+        },
+
         value: {
-            value: function () { return []; },
+            value: function () { return {}; },
             notify: true,
+        },
+
+        slidable: {
+            type: Boolean,
+            value: false,
+            reflectToAttribute: true,
+        },
+
+        folded: {
+            type: Boolean,
+            value: true,
         },
     },
 
-    observers: [
-        'sizeChanged(value.length)'
-    ],
+    ready: function () {
+        this._initFocusable(this.$.focus);
+    },
 
-    sizeChanged: function ( changeRecord ) {
-        this.$.input.value = this.value.length;
+    _nameText: function ( name, attrs ) {
+        if ( attrs && attrs.displayName ) {
+            return attrs.displayName;
+        }
+        else if ( name ) {
+            return EditorUI.toHumanText(name);
+        }
+
+        return '(Anonymous)';
+    },
+
+    _nameClass: function ( name, attrs ) {
+        if ( attrs && attrs.displayName ) {
+            return 'name flex-1';
+        }
+        else if ( name ) {
+            return 'name flex-1';
+        }
+
+        return 'name anonymous flex-1';
+    },
+
+    _onFocusIn: function ( event ) {
+        this._setFocused(true);
+        this.$.field.editing = true;
+    },
+
+    _onFocusOut: function ( event ) {
+        this._setFocused(false);
+        this.$.field.editing = false;
+    },
+
+    _onMouseDown: function ( event ) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        var el = EditorUI.getFirstFocusableChild( this.$.field );
+        if ( el )
+            el.focus();
+
+        if (this.slidable) {
+            var lastValue = this.value;
+
+            var min = Number.NEGATIVE_INFINITY;
+            if ( typeof this.attrs.min === 'number' ) min = this.attrs.min;
+
+            var max = Number.POSITIVE_INFINITY;
+            if ( typeof this.attrs.max === 'number' ) max = this.attrs.max;
+
+            EditorUI.startDrag('ew-resize', event,function (event, dx, dy, offsetx, offsety) {
+                this.value = Math.clamp(lastValue + offsetx, min, max);
+            }.bind(this),null);
+        }
+    },
+
+    _onFieldMouseDown: function ( event ) {
+        event.stopPropagation();
+        // don't do any propagation if we mouse down on field
+    },
+
+    _onKeyDown: function (event) {
+        // enter
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            var el = EditorUI.getFirstFocusableChild( this.$.field );
+            if ( el )
+                el.focus();
+        }
+    },
+
+    _onDisabledChanged: function ( event ) {
+        var children = Polymer.dom(this.$.field).children;
+        for ( var i = 0; i < children.length; ++i ) {
+            var childEL = children[i];
+            if ( childEL.disabled !== undefined ) {
+                childEL.disabled = event.detail.value;
+            }
+        }
+    },
+
+    _onFoldMouseDown: function ( event ) {
+        event.stopPropagation();
+        event.preventDefault();
+    },
+
+    _onFoldClick: function ( event ) {
+        event.stopPropagation();
+
+        if ( event.which !== 1 )
+            return;
+
+        this.folded = !this.folded;
+    },
+
+    _foldClass: function ( folded ) {
+        if ( folded ) {
+            return 'fa fa-caret-right fold flex-none';
+        }
+
+        return 'fa fa-caret-down fold flex-none';
     },
 });
