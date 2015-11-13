88313f6f67de4cd02883e8181e25f3dda4286665
Johnny Wu <KarasAya@gmail.com>
2015-11-13 19:08:00 +0800

    remove init

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 module.exports = {
     load: function () {
-        require('./init');
     },
 
     unload: function () {
-        [
-            'null-or-undefined',
-            'error',
-            'Boolean',
-            'Integer',
-            'Float',
-            'Number',
-            'String',
-            'Enum',
-            'cc.Vec2',
-            'cc.Color',
-            'cc.RawAsset',
-            'cc.Asset',
-            'cc.Texture2D',
-            'cc.BitmapFont',
-            'cc.TTFFont',
-            'cc.AudioClip',
-            'cc.NodeWrapper',
-        ].forEach(function ( item ) {
-            delete Editor.properties[item];
-        });
     },
 
     'ui-property:open': function () {
