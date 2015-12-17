930181ed13659ee6016605ffbb53620d964209fb
Johnny Wu <KarasAya@gmail.com>
2015-12-17 20:20:25 +0800

    fix array name not showing correctly

--- widget/value-prop/value-prop.js
+++ widget/value-prop/value-prop.js
@@ -2,116 +2,110 @@
 
 Editor.registerElement({
 
-    behaviors: [EditorUI.focusable],
+  behaviors: [EditorUI.focusable],
 
-    listeners: {
-        'focus': '_onFocus',
-        'blur': '_onBlur',
-        'focusin': '_onFocusIn',
-        'focusout': '_onFocusOut',
-        'mousedown': '_onMouseDown',
-        'keydown': '_onKeyDown',
+  listeners: {
+    'focus': '_onFocus',
+    'blur': '_onBlur',
+    'focusin': '_onFocusIn',
+    'focusout': '_onFocusOut',
+    'mousedown': '_onMouseDown',
+    'keydown': '_onKeyDown',
+  },
+
+  properties: {
+    prop: {
+      value () {
+        return {
+          path: '',
+          type: '',
+          name: '',
+          attrs: {},
+          value: null,
+        };
+      },
+      notify: true,
     },
 
-    properties: {
-        prop: {
-            value: function () {
-                return {
-                    path: '',
-                    type: '',
-                    name: '',
-                    attrs: {},
-                    value: null,
-                };
-            },
-            notify: true,
-        },
-
-        slidable: {
-            type: Boolean,
-            value: false,
-            reflectToAttribute: true,
-        },
+    slidable: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
     },
+  },
 
-    ready: function () {
-        this._initFocusable(this);
-    },
+  ready () {
+    this._initFocusable(this);
+  },
 
-    _nameText: function ( name, attrs ) {
-        if ( attrs && attrs.displayName ) {
-            return attrs.displayName;
-        }
-        else if ( name ) {
-            return EditorUI.toHumanText(name);
-        }
+  _nameText ( name ) {
+    if ( name ) {
+      return EditorUI.toHumanText(name);
+    }
 
-        return '(Anonymous)';
-    },
+    return '(Anonymous)';
+  },
 
-    _nameClass: function ( name, attrs ) {
-        if ( attrs && attrs.displayName ) {
-            return 'name flex-1';
-        }
-        else if ( name ) {
-            return 'name flex-1';
-        }
+  _nameClass ( name ) {
+    if ( name ) {
+      return 'name flex-1';
+    }
 
-        return 'name anonymous flex-1';
-    },
+    return 'name anonymous flex-1';
+  },
 
-    _onFocusIn: function ( event ) {
-        this._setFocused(true);
-        this.$.field.editing = true;
-    },
+  _onFocusIn () {
+    this._setFocused(true);
+    this.$.field.editing = true;
+  },
 
-    _onFocusOut: function ( event ) {
-        this._setFocused(false);
-        this.$.field.editing = false;
-    },
+  _onFocusOut () {
+    this._setFocused(false);
+    this.$.field.editing = false;
+  },
 
-    _onMouseDown: function ( event ) {
-        event.preventDefault();
-        event.stopPropagation();
+  _onMouseDown ( event ) {
+    event.preventDefault();
+    event.stopPropagation();
 
-        var el = EditorUI.getFirstFocusableChild( this.$.field );
-        if ( el )
-            el.focus();
+    var el = EditorUI.getFirstFocusableChild( this.$.field );
+    if ( el )
+      el.focus();
 
-        if (this.slidable) {
-            var lastValue = this.prop.value;
+    if (this.slidable) {
+      var lastValue = this.prop.value;
 
-            var min = Number.NEGATIVE_INFINITY;
-            if ( typeof this.prop.attrs.min === 'number' ) min = this.prop.attrs.min;
+      var min = Number.NEGATIVE_INFINITY;
+      if ( typeof this.prop.attrs.min === 'number' ) min = this.prop.attrs.min;
 
-            var max = Number.POSITIVE_INFINITY;
-            if ( typeof this.prop.attrs.max === 'number' ) max = this.prop.attrs.max;
+      var max = Number.POSITIVE_INFINITY;
+      if ( typeof this.prop.attrs.max === 'number' ) max = this.prop.attrs.max;
 
-            EditorUI.startDrag('ew-resize', event, (event, dx, dy, offsetx, offsety) => {
-                this.set('prop.value', Math.clamp(lastValue + offsetx, min, max));
-            }, () => {
-                this.async(() => {
-                    this.fire('end-editing');
-                },1);
-            });
-        }
-    },
+      EditorUI.startDrag('ew-resize', event, (event, dx, dy, offsetx, offsety) => {
+        this.set('prop.value', Math.clamp(lastValue + offsetx, min, max));
+      }, () => {
+        this.async(() => {
+          this.fire('end-editing');
+        },1);
+      });
+    }
+  },
 
-    _onFieldMouseDown: function ( event ) {
-        event.stopPropagation();
-        // don't do any propagation if we mouse down on field
-    },
+  _onFieldMouseDown ( event ) {
+    event.stopPropagation();
+    // don't do any propagation if we mouse down on field
+  },
 
-    _onKeyDown: function (event) {
-        // enter
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            event.stopPropagation();
+  _onKeyDown (event) {
+    // enter
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      event.stopPropagation();
 
-            var el = EditorUI.getFirstFocusableChild( this.$.field );
-            if ( el ) {
-                el.focus();
-            }
-        }
-    },
+      var el = EditorUI.getFirstFocusableChild( this.$.field );
+      if ( el ) {
+        el.focus();
+      }
+    }
+  },
 });
